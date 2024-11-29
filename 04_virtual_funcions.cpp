c2b9e57a35588905c5fbc3372f570298cdc1f77c
priyanshu-chauhan-14 <priyanshuchauhan95088@gmail.com>
2024-11-29 14:32:36 +0530

    adding 4

--- 04_virtual_funcions.cpp
+++ 04_virtual_funcions.cpp
@@ -1,3 +1,24 @@
 /* 
 Default Virtual Functions: Implement a base class Game with a virtual function start() having a default implementation that prints "Game starting!". Override start() in a derived class Chess to print "Chess match starting!". Demonstrate the usage of the default implementation.
 */
+#include<iostream>
+using namespace std;
+class Game{
+ protected:
+
+public:
+    virtual void start(){
+        cout<<"Game is starting..."<<endl;
+    }
+};
+class chess{
+    protected:
+    public:
+    void start(){
+        cout<<"Chess match starting !"<<endl;
+    }
+};
+int main(){
+ 
+return 0;
+}
